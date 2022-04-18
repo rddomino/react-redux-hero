0aee226ed876bdfd51c1ceaf6159fb28a4d4984d
rddomino <seltos@bk.ru>
2022-04-18 19:41:49 +0600

    Добавление Redux-Thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+import ReduxThunk from 'redux-thunk';
 
 const stringMiddleware = () => (next) => (action) => {
     if (typeof action === 'string') {
@@ -13,7 +14,7 @@ const stringMiddleware = () => (next) => (action) => {
 
 const store = createStore(combineReducers({heroes, filters}), 
                             compose(
-                                applyMiddleware(stringMiddleware),
+                                applyMiddleware(ReduxThunk, stringMiddleware),
                                 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
                             ));
 
